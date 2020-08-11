b98a88328664a022dc7bcd987e32d7feb2cc9959
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 18:29:31 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,10 @@
 module findable
 
-  def self.find_by_name(name)
+  def find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
 
-  def self.find_by_name(name)
+  def find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
 
