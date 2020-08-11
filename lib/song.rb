93f334324aac9ad6fe05575f522e009df4c29a89
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 18:50:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,9 @@ include Paramable
   # @@songs = []
   @@all = []
 
-  def initialize
-    self.class.all << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
